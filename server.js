5a084ace62215e2878b1dff9063c122e07ca2f2c
mariagriggi <maria.rondon3@sou.ufmt.br>
2025-03-14 17:15:17 -0400

    Atualizando configuração do server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import { PrismaClient } from '@prisma/client'
+import cors from 'cors';
 
 const app = express()
 const prisma = new PrismaClient()
+
+app.use(express.json())
+app.use(cors());
 app.use(express.json())
 
 const users = []
